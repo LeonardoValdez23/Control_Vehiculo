0420f34cae5b98b1fd237af4e2cc37e9cf8cfa5f
LeonardoValdez23 <160821059+LeonardoValdez23@users.noreply.github.com>
2025-05-28 20:27:59 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ function iniciarBotones() {
         status: clave // Aquí solo se envía la CLAVE
       };
 
-      fetch('http://34.203.217.61/api/devices', {
+      fetch('http://18.205.106.20/api/devices', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
